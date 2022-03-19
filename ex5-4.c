553cc248440a2042117bd6c7dcabe36dd84e75c1
chiyanglin-AStar <chiyanglin@gmail.com>
2022-03-19 08:51:33 +0000

    update

--- ex5-4.c
+++ ex5-4.c
@@ -2,8 +2,8 @@
 int main(void){
     int i,j;
     
-    for(i=1;i<4;i++){
-        for(j=1;j<4;j++){
+    for(i=1;i<10;i++){
+        for(j=1;j<10;j++){
             printf(" %d x % d =%d \n",i,j,i*j);
         }
     }
